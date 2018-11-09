71320846e3399f8372b19661215d79773521bb63
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-09 23:20:59 +0000

    automatically backed up by learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,8 +3,7 @@ module PostsHelper
     if @post.author.nil?
       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
     else
-      hidden_field_tag "post[author_id]", post.author.id
+      hidden_field_tag "post[author_id]", post.author_id
     end
   end
-
 end
